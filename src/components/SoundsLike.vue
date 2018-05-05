e19d25d704fbacbcbd98b9c47ce4ffb392a902be
Kelly Colht <colhk@seattleu.edu>
2018-05-04 22:47:39 -0700

    New component

---
+++ src/components/SoundsLike.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="soundslike">
+    <h1>Words: They Sometimes Sound Like Other Words</h1>
+    <ol>
+      <li>
+        <router-link v-bind:to="{name: 'SoundsLike'}">Sounds Like</router-link>
+      </li>
+      &#8226;
+      <li>
+        <router-link v-bind:to="{name: 'Rhymesaurus'}">Rhymesaurus</router-link>
+      </li>
+    </ol>
+
+    <form v-on:submit.prevent="findWords">
+      <p>Find words that sound like
+        <input type="text" v-model="word">
+        <button type="submit">Search</button>
+      </p>
+    </form>
+    <ul v-if="results && results.length > 0" class="results">
+      <li v-for="item in results" class="item">
+        <p>
+          <strong>{{ item.word }}</strong>
+        </p>
+        <p>{{ item.score }}</p>
+      </li>
+    </ul>
+
+    <div v-else-if="results && results.length === 0" class="no-results">
+      <h2>No Words Found</h2>
+      <p>Please adjust your search to find more words.</p>
+    </div>
+
+    <ul v-if="errors.length > 0" class="errors">
+      <li v-for="error in errors">
+        {{ error.message}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'SoundsLike',
+  data () {
+    return {
+      results: null,
+      errors: [],
+      word: '',
+    }
+  },
+  methods: {
+    findWords: function() {
+      axios.get('https://api.datamuse.com/words', {
+        params: {
+        rel_hom: this.word
+        }
+      })
+      .then( response => {
+        this.results = response.data;
+      })
+      .catch( error => {
+        this.errors.push(error);
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.soundslike {
+  font-size: 1.4rem;
+}
+input[type="text"]{
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  border-bottom: 1px solid #333;
+  width: 300px;
+  font-size: 1.4rem;
+  color: #2c3e50;
+  font-weight: 300;
+  background: rgba(0,0,0,0.02);
+  padding: 0.5rem;
+}
+button{
+  background: #333;
+  padding: 0.5rem;
+  font-weight: 300;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  font-size: 1.4rem;
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul.results {
+  list-style-type: none;
+  padding: 0;
+}
+.results li {
+  display: inline-block;
+  margin: 10px;
+  border: solid 1px #333;
+  padding: 0.5rem;
+  width: 200px;
+  min-height: 100px;
+  color: #fff;
+  background: rgba(0,0,0,0.7);
+}
+ul.errors {
+  list-style-type: none;
+}
+ol li {
+  list-style-type: none;
+  display: inline;
+}
+.errors li {
+  border: 1px solid red;
+  color: red;
+  padding: 0.5rem;
+  margin: 10px 0;
+}
+a {
+  color: #42b983;
+}
+</style>
